9912d5abc4f315fb576690da820af98b54e260b7
Franz <quemejavi@gmail.com>
2023-02-14 09:49:32 -0600

    Correcciones de espacios y carperas modulos

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print('Nombre: Francisco Javier Queme Dominguez'
-      'Carnet: 1552023'
-      'Carrera: Ingenieria en informatica y sistemas'
-      'Primer ciclo')
+print('Nombre: Francisco Javier Queme Dominguez Carnet: 1552023 Carrera: Ingenieria en informatica y sistemas Primer ciclo')
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
